8af205cafb789ce9f3b2dd3ec1a9dd69f9a21a9d
Trevor Jones <tjones009@yahoo.com>
2020-02-26 23:24:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+    add1 = num1 + num2
+  puts add1
+  return add1
 end
 
 def subtraction(num1, num2)
-
+    sub1 = num1 - num2
+  puts sub1
+  return sub1
 end
 
 def division(num1, num2)
-
+    div1 = num1 / num2
+  puts div1
+  return div1
 end
 
 def multiplication(num1, num2)
-
+    mult1 = num1 * num2
+  puts mult1
+  return mult1
 end
 
 def modulo(num1, num2)
-
+    modulo1 = num1.remainder(num2)
+  puts modulo1
+  return modulo1
 end
 
 def square_root(num)
-
+   math.sqrt(num) = ss
+  puts ss
+  return ss
 end
